05f738d08fa06e2ae72b00759349b7b5d21d380e
Amos Wenger <amoswenger@gmail.com>
2016-11-24 17:55:48 +0100

    better reproduction, shows off lack of certificate-error for net requests and presence thereof for BrowserWindow

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 "use strict";
 
-const {app, session, net} = require("electron");
+const {app, session, net, BrowserWindow} = require("electron");
 
-const httpProxy = process.env.HTTP_PROXY;
+const httpProxy = process.env.http_proxy;
+const testUrl = "https://itch.io/country"
 
 init();
 
 function init () {
   console.log("electron version: ", process.versions.electron);
 
+  process.on("uncaughtException", (e) => {
+    console.log("< uncaught exception: ", e);
+  });
+
+  app.on("certificate-error", (ev, wc, url, error) => {
+    console.log("< certificate error: ", error);
+  });
+
   if (!httpProxy) {
-    console.log("Please set the HTTP_PROXY environment variable. Example: HTTP_PROXY=localhost:8888");
+    console.log("Please set the http_proxy environment variable. Example: http_proxy=localhost:8888");
     process.exit(1);
   }
 
@@ -20,7 +29,7 @@ function init () {
 function makeRequest (cb) {
   const req = net.request({
     method: "get",
-    url: "https://itch.io/country"
+    url: testUrl
   })
 
   let response;
@@ -35,19 +44,35 @@ function makeRequest (cb) {
     });
   })
 
+  let timedOut = false;
+  let timeoutHandle = setTimeout(function () {
+    timedOut = true;
+    cb("<timed out>")
+  }, 3 * 1000)
+
   req.on("close", function () {
-    cb(body);
+    clearTimeout(timeoutHandle);
+    if (timedOut) {
+      return
+    }
+
+    if (body === "") {
+      cb("<empty>");
+    } else {
+      cb(body);
+    }
   });
   req.end();
 }
 
 function appReady () {
-  console.log("app ready! making proxyless request...");
+  console.log("✓ app ready!")
+  console.log("> making proxyless request...");
   makeRequest(proxylessRequestDone);
 }
 
 function proxylessRequestDone (body) {
-  console.log(`proxyless request gave: ${body}`)
+  console.log(`< proxyless request gave: ${body}`)
 
   session.defaultSession.setProxy({
     proxyRules: httpProxy
@@ -55,11 +80,19 @@ function proxylessRequestDone (body) {
 }
 
 function proxySet () {
-  console.log("proxy set! making proxyfull request...");
+  console.log("✓ proxy set!");
+  console.log("> making proxyfull request...");
   makeRequest(proxyfullRequestDone);
 }
 
 function proxyfullRequestDone (body) {
-  console.log(`proxyfull request gave: ${body}`)
+  console.log(`< proxyfull request gave: ${body}`)
+  console.log(`> opening BrowserWindow...`)
+
+  const win = new BrowserWindow({
+    width: 400, height: 400,
+    show: true,
+  });
+  win.loadURL(testUrl);
 }
 
